9b4c7111dcd04062257375ac362731fad2791dfa
Lucy Jiang <lucjia@fb.com>
2019-06-27 17:35:55 -0700

    posterViewController

---
+++ flix/View Controllers/PosterViewController.h
@@ -0,0 +1,17 @@
+//
+//  PosterViewController.h
+//  flix
+//
+//  Created by lucjia on 6/27/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PosterViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flix/View Controllers/PosterViewController.m
@@ -0,0 +1,32 @@
+//
+//  PosterViewController.m
+//  flix
+//
+//  Created by lucjia on 6/27/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import "PosterViewController.h"
+
+@interface PosterViewController ()
+
+@end
+
+@implementation PosterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
